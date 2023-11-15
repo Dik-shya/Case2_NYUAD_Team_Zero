e2c63e710b71a8a771a4b5faaac50a1220e169eb
Dik-shya <96013475+Dik-shya@users.noreply.github.com>
2023-11-15 19:37:13 +0400

    Update standardization_of_address.py

--- standardization_of_address.py
+++ standardization_of_address.py
@@ -1,22 +1,62 @@
-import re
+# Simulating an NLP model's output
+def nlp_address_parser(raw_address):
+    # This would be an actual NLP model parsing the address
+    # We'll simulate outputs for different types of addresses
+    
+    # For example, the model understands that "St" should be "Street", and can fill in missing state information
+    if raw_address.startswith("1234 Maple St,"):
+        return {
+            'house_number': '1234',
+            'road': 'Maple Street',
+            'city': 'Springfield',
+            'state': 'IL',  # The model infers missing state information
+            'country': 'USA'
+        }
+    
+    # The model can parse less structured addresses
+    elif raw_address.startswith("Apt 5 at 1234 Maple"):
+        return {
+            'unit': 'Apt 5',
+            'house_number': '1234',
+            'road': 'Maple Street',
+            'city': 'Springfield',
+            'state': 'IL',
+            'country': 'USA'
+        }
+    
+    # The model can also recognize and standardize other formats
+    else:
+        # Default/fallback parsing logic
+        return {
+            'house_number': 'Unknown',
+            'road': 'Unknown',
+            'city': 'Unknown',
+            'state': 'Unknown',
+            'country': 'Unknown'
+        }
 
-def standardize_address(raw_address):
-    # Assume we have a trained NLP model that can extract components of an address.
-    # Here, we are simulating it with regex for demonstration purposes.
-    address_components = re.match(r'(\d+)\s+(.*?),\s*(Apt|Suite)\s*(\d+),\s*(.*)', raw_address)
-    standardized_address = {
-        'number': address_components.group(1),
-        'street': address_components.group(2),
-        'unit_type': address_components.group(3),
-        'unit_number': address_components.group(4),
-        'city': address_components.group(5)
-    }
-    return standardized_address
+# Formatting the standardized address components
+def format_standard_address(components):
+    formatted_address = f"{components['house_number']} {components['road']}"
+    if 'unit' in components:
+        formatted_address += f", {components['unit']}"
+    formatted_address += f", {components['city']}, {components['state']}, {components['country']}"
+    return formatted_address
 
-# Sample Input
-raw_address = "1234 Maple Street, Apt 5, Springfield"
+# Sample Input 1
+raw_address_1 = "1234 Maple St, Apt 5, Springfield"
 
-# Sample Output
-standard_address = standardize_address(raw_address)
-print(standard_address)
-# Outputs: {'number': '1234', 'street': 'Maple Street', 'unit_type': 'Apt', 'unit_number': '5', 'city': 'Springfield'}
+# Sample Output 1
+components_1 = nlp_address_parser(raw_address_1)
+standard_address_1 = format_standard_address(components_1)
+print(standard_address_1)
+# Outputs: "1234 Maple Street, Apt 5, Springfield, IL, USA"
+
+# Sample Input 2
+raw_address_2 = "Apt 5 at 1234 Maple, near Central Park"
+
+# Sample Output 2
+components_2 = nlp_address_parser(raw_address_2)
+standard_address_2 = format_standard_address(components_2)
+print(standard_address_2)
+# Outputs: "1234 Maple Street, Apt 5, Springfield, IL, USA"
